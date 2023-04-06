5df982275d889a900952e3c432934b4020550f54
Bruno Rocha <bruno.miguel.rocha@hotmail.com>
2023-04-06 16:03:11 +0100

    changes names for april 23

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let padding = { top: 20, right: 40, bottom: 0, left: 0 },
+var padding = { top: 20, right: 40, bottom: 0, left: 0 },
   w = 600 - padding.left - padding.right,
   h = 600 - padding.top - padding.bottom,
   r = Math.min(w, h) / 2,
@@ -9,242 +9,226 @@ let padding = { top: 20, right: 40, bottom: 0, left: 0 },
   turn = false;
 color = d3.scale.category20();
 
-
-/* Attention: The following logic is the same for the Data Wheel: https://github.com/lzaquine/wheel_data */
-
-/* Change name/picture of each student */
-
-let data = [
+var data = [
   {
-    label: "Alexandre A.", /* Student Name */
+    label: 'Ana R.',
     value: 1,
-    question: "Alexandre", /* Name that is going to show up on the screen */
-    pic: "Alunos_Jan23/Alexandre.png", /* Picture of the student */
-  },
+    question: 'Ana R.',
+    pic: 'Alunos_Jan23/Bruno.jpeg',
+  }, // padding
+  { label: 'Bernardo A.', 
+    value: 2, 
+    question: 'Bernardo A.', 
+    pic: 'Alunos_Jan23/Caio.jpeg', 
+  }, //font-family
   {
-    label: "André L.", 
-    value: 2,
-    question: "André",
-    pic: "Alunos_Jan23/Andre.jpeg",
-  },
-  {
-    label: "Assebe K.",
+    label: 'Cristian K.',
     value: 3,
-    question: "Assebe",
-    pic: "Alunos_Jan23/Assebe.jpeg",
-  },
+    question: 'Cristian K.',
+    pic: 'Alunos_Jan23/Leonor.png',
+  }, //color
   {
-    label: "Caio M.",
+    label: 'Elnaz F.',
     value: 4,
-    question: "Caio",
-    pic: "Alunos_Jan23/Caio.jpeg",
-  },
+    question: 'Elnaz F.',
+    pic: 'Alunos_Jan23/Marta.jpeg',
+  }, //font-weight
   {
-    label: "Caroline K.",
+    label: 'Erik K.',
     value: 5,
-    question: "Caroline",
-    pic: "Alunos_Jan23/Caroline.png",
-  },
+    question: 'Erik K.',
+    pic: 'Alunos_Jan23/Milena.jpeg',
+  }, //font-size
   {
-    label: "Duarte F.",
+    label: 'Francisco B.',
     value: 6,
-    question: "Duarte",
-    pic: "Alunos_Jan23/Duarte.jpeg",
+    question: 'Francisco B.',
+    pic: 'Alunos_Jan23/Ricardo.jpeg',
+  }, //nesting
+  { label: 'Inception', 
+    value: 7, 
+    question: "Teacher's choice", 
+    pic: 'Alunos_Jan23/Rui.png', 
   },
   {
-    label: "Déborah L.",
-    value: 7,
-    question: "Déborah",
-    pic: "Alunos_Jan23/Deborah.png",
-  },
-  {
-    label: "Erika G.",
+    label: 'Guglielmo G.',
     value: 8,
-    question: "Erika",
-    pic: "Alunos_Jan23/Erika.jpeg",
-  },
+    question: 'Guglielmo G.',
+    pic: 'Alunos_Jan23/Sanaz.jpeg',
+  }, //sans-serif
   {
-    label: "Eveline C.",
+    label: 'Guilherme D.',
     value: 9,
-    question: "Eveline",
-    pic: "Alunos_Jan23/Eveline.png",
+    question: 'Guilherme D.',
+    pic: 'Alunos_Jan23/Thiago.jpeg',
   },
   {
-    label: "Farid C.",
+    label: 'Gustavo C.',
     value: 10,
-    question: "Farid",
-    pic: "Alunos_Jan23/Farid.png",
+    question: 'Gustavo C.',
+    pic: 'Alunos_Jan23/Vasco.png',
   },
   {
-    label: "Francisco P.",
+    label: 'Francisco C.',
     value: 11,
-    question: "Francisco",
-    pic: "Alunos_Jan23/Francisco.png",
+    question: 'Francisco C.',
+    pic: 'Alunos_Jan23/Veronika.jpeg',
   },
   {
-    label: "Gabriel G.",
+    label: 'Guy D.',
     value: 12,
-    question: "Gabriel",
-    pic: "Alunos_Jan23/Gabriel.png",
+    question: 'Guy D.',
+    pic: 'Alunos_Jan23/Abdul.png',
   },
   {
-    label: "Joana G.",
+    label: 'Henrique P.',
     value: 13,
-    question: "Joana",
-    pic: "Alunos_Jan23/Joana.jpeg",
+    question: 'Henrique P.',
+    pic: 'Alunos_Jan23/Abdul.png',
   },
   {
-    label: "José L.",
+    label: 'João C.',
     value: 14,
-    question: "José",
-    pic: "Alunos_Jan23/JoseL.png",
+    question: 'João C.',
+    pic: 'Alunos_Jan23/Abdul.png',
   },
   {
-    label: "João C.",
+    label: 'João M.',
     value: 15,
-    question: "João Carneiro",
-    pic: "Alunos_Jan23/JoaoC.png",
+    question: 'João M.',
+    pic: 'Alunos_Jan23/Abdul.png',
   },
   {
-    label: "João R.",
+    label: 'Joie S.',
     value: 16,
-    question: "João Raposo",
-    pic: "Alunos_Jan23/JoaoR.jpeg",
+    question: 'Joie S.',
+    pic: 'Alunos_Jan23/Abdul.png',
   },
   {
-    label: "Maria C.",
+    label: 'Lucas T.',
     value: 17,
-    question: "Maria",
-    pic: "Alunos_Jan23/Maria.jpeg",
+    question: 'Lucas T.',
+    pic: 'Alunos_Jan23/Abdul.png',
   },
   {
-    label: "Mariana F.",
+    label: 'Margarida P.',
     value: 18,
-    question: "Mariana",
-    pic: "Alunos_Jan23/Mariana.jpeg",
+    question: 'Margarida P.',
+    pic: 'Alunos_Jan23/Abdul.png',
   },
   {
-    label: "Marisha D.",
+    label: 'Marisa P.',
     value: 19,
-    question: "Marisha",
-    pic: "Alunos_Jan23/Marisha.jpeg",
+    question: 'Marisa P.',
+    pic: 'Alunos_Jan23/Abdul.png',
   },
   {
-    label: "Miguel J.",
+    label: 'Niroj G.',
     value: 20,
-    question: "Miguel Jesus",
-    pic: "Alunos_Jan23/MiguelJ.png",
+    question: 'Niroj G.',
+    pic: 'Alunos_Jan23/Abdul.png',
   },
   {
-    label: "Paulo C.",
+    label: 'Nuno D.',
+    value: 21,
+    question: 'Nuno D.',
+    pic: 'Alunos_Jan23/Abdul.png',
+  },
+  {
+    label: 'Pedro N.',
     value: 22,
-    question: "Paulo",
-    pic: "Alunos_Jan23/Paulo.png",
+    question: 'Pedro N.',
+    pic: 'Alunos_Jan23/Abdul.png',
   },
   {
-    label: "Pedro L.",
+    label: 'Renato P.',
     value: 23,
-    question: "Pedro",
-    pic: "Alunos_Jan23/Pedro.jpeg",
+    question: 'Renato P.',
+    pic: 'Alunos_Jan23/Abdul.png',
   },
   {
-    label: "Rafaela U.",
+    label: 'Robson B.',
     value: 24,
-    question: "Rafaela",
-    pic: "Alunos_Jan23/Rafaela.jpeg",
+    question: 'Robson B.',
+    pic: 'Alunos_Jan23/Abdul.png',
   },
   {
-    label: "Tiago R.",
+    label: 'Roshan P.',
     value: 25,
-    question: "Tiago",
-    pic: "Alunos_Jan23/Tiago.jpeg",
+    question: 'Roshan P.',
+    pic: 'Alunos_Jan23/Abdul.png',
   },
   {
-    label: "Tomás B.",
+    label: 'Tomás M.',
     value: 26,
-    question: "Tomás",
-    pic: "Alunos_Jan23/Tomas.png",
+    question: 'Tomás M.',
+    pic: 'Alunos_Jan23/Abdul.png',
   },
-  {
-    label: "Vanessa V.",
-    value: 27,
-    question: "Vanessa",
-    pic: "Alunos_Jan23/Vanessa.jpeg",
+  { label: 'Victoria A.', 
+    value: 27, 
+    question: 'Victoria A.', 
+    pic: 'Alunos_Jan23/Pedro.png',
   },
 ];
-let svg = d3
-  .select("#chart")
-  .append("svg")
+var svg = d3
+  .select('#chart')
+  .append('svg')
   .data([data])
-  .attr("width", w + padding.left + padding.right)
-  .attr("height", h + padding.top + padding.bottom);
-let container = svg
-  .append("g")
-  .attr("class", "chartholder")
-  .attr(
-    "transform",
-    "translate(" + (w / 2 + padding.left) + "," + (h / 2 + padding.top) + ")"
-  );
-let vis = container.append("g");
+  .attr('width', w + padding.left + padding.right)
+  .attr('height', h + padding.top + padding.bottom);
+var container = svg
+  .append('g')
+  .attr('class', 'chartholder')
+  .attr('transform', 'translate(' + (w / 2 + padding.left) + ',' + (h / 2 + padding.top) + ')');
+var vis = container.append('g');
 
-let pie = d3.layout
+var pie = d3.layout
   .pie()
   .sort(null)
   .value(function (d) {
     return 1;
   });
-let arc = d3.svg.arc().outerRadius(r);
-let arcs = vis
-  .selectAll("g.slice")
-  .data(pie)
-  .enter()
-  .append("g")
-  .attr("class", "slice");
+var arc = d3.svg.arc().outerRadius(r);
+var arcs = vis.selectAll('g.slice').data(pie).enter().append('g').attr('class', 'slice');
 
 arcs
-  .append("path")
-  .attr("fill", function (d, i) {
+  .append('path')
+  .attr('fill', function (d, i) {
     return color(i);
   })
-  .attr("d", function (d) {
+  .attr('d', function (d) {
     return arc(d);
   });
 arcs
-  .append("text")
-  .attr("transform", function (d) {
+  .append('text')
+  .attr('transform', function (d) {
     d.innerRadius = 0;
     d.outerRadius = r;
     d.angle = (d.startAngle + d.endAngle) / 2;
     return (
-      "rotate(" +
-      ((d.angle * 180) / Math.PI - 90) +
-      ")translate(" +
-      (d.outerRadius - 10) +
-      ")"
+      'rotate(' + ((d.angle * 180) / Math.PI - 90) + ')translate(' + (d.outerRadius - 10) + ')'
     );
   })
-  .attr("text-anchor", "end")
+  .attr('text-anchor', 'end')
   .text(function (d, i) {
     return data[i].label;
   });
-container.on("click", spin);
+container.on('click', spin);
 function spin(d) {
   turn = true;
-  container.on("click", null);
+  container.on('click', null);
   if (oldpick.length == data.length) {
-    console.log("done");
-    container.on("click", null);
+    console.log('done');
+    container.on('click', null);
     return;
   }
-
-  /* Song method.  */
   song.play();
-  let ps = 360 / data.length,
+  var ps = 360 / data.length,
     pieslice = Math.round(1440 / data.length),
-    /* RNG Velocity */
+    /* Velocidade rng */
     rng = Math.floor(Math.random() * 2880 + 360);
 
-  /* Rotation Velocity */
+  /* VELOCIDADE DA ROTACAO */
   rotation = Math.round(rng / ps) * ps * 2;
 
   picked = Math.round(data.length - (rotation % 360) / ps);
@@ -258,68 +242,59 @@ function spin(d) {
   rotation += 90 - Math.round(ps / 2);
   vis
     .transition()
-    /* Duration of each spin */
-    .duration(8000) /* I always cropped the audio to 7-8 seconds to use 8000 here */
-    .attrTween("transform", rotTween)
-    .each("end", function () {
-      if (data[picked].label === "Miguel L.") {
-        data[picked].label === "NOT HERE";
-      }
-      d3.select(".slice:nth-child(" + (picked + 1) + ") path").attr(
-        "fill",
-        "#111"
-      );
-      d3.select("#question h1").text(data[picked].question);
-      d3.select("#pic img").attr("src", data[picked].pic);
+    /* DURACAO DO TEMPO DELA RODANDO */
+    .duration(7500)
+    .attrTween('transform', rotTween)
+    .each('end', function () {
+      d3.select('.slice:nth-child(' + (picked + 1) + ') path').attr('fill', '#111');
+      d3.select('#question h1').text(data[picked].question);
+      d3.select('#pic img').attr('src', data[picked].pic);
       oldrotation = rotation;
+      console.log(data[picked].label);
+      console.log([picked]);
+      console.log(data[picked].pic);
       turn = false;
-      container.on("click", spin);
+      console.log(turn);
+      container.on('click', spin);
     });
 }
-
-/* Arrow */
+//arrow
 svg
-  .append("g")
-  .attr(
-    "transform",
-    "translate(" + (w + 200) + "," + (h / 2 + padding.top) + ")"
-  )
-  .append("path")
-  .attr("d", "M-" + r * 0.8 + ",0L0," + r * 0.4 + "L0,-" + r * 0.4 + "Z")
-  .style({ fill: "red" });
-/* Draw spin circle */
+  .append('g')
+  .attr('transform', 'translate(' + (w + 200) + ',' + (h / 2 + padding.top) + ')')
+  .append('path')
+  .attr('d', 'M-' + r * 0.8 + ',0L0,' + r * 0.4 + 'L0,-' + r * 0.4 + 'Z')
+  .style({ fill: 'red' });
+//draw spin circle
 /* container.append("circle")
-            .attr("cx", 0)
-            .attr("cy", 0)
-            .attr("r", 60)
-            .style({"fill":"white","cursor":"pointer"}); */
+                    .attr("cx", 0)
+                    .attr("cy", 0)
+                    .attr("r", 60)
+                    .style({"fill":"white","cursor":"pointer"}); */
 container
-  .append("image")
-  .attr("xlink:href", "/assets/ironhackpt-removebg-preview.png") /* Image on the center of the wheel. To change size/position, lines 299 to 302 */
-  .attr("x", -110)
-  .attr("y", -110)
-  .attr("width", 225)
-  .attr("height", 225)
-  .style({ cursor: "pointer" });
+  .append('image')
+  .attr('xlink:href', '/assets/ironhackpt-removebg-preview.png')
+  .attr('x', -110)
+  .attr('y', -110)
+  .attr('width', 225)
+  .attr('height', 225)
+  .style({ cursor: 'pointer' });
 
 function rotTween(to) {
-  let i = d3.interpolate(oldrotation % 360, rotation);
+  var i = d3.interpolate(oldrotation % 360, rotation);
   return function (t) {
-    return "rotate(" + i(t) + ")";
+    return 'rotate(' + i(t) + ')';
   };
 }
 
 function getRandomNumbers() {
-  let array = new Uint16Array(1000);
-  let scale = d3.scale.linear().range([360, 1440]).domain([0, 100000]);
-  if (
-    window.hasOwnProperty("crypto") &&
-    typeof window.crypto.getRandomValues === "function"
-  ) {
+  var array = new Uint16Array(1000);
+  var scale = d3.scale.linear().range([360, 1440]).domain([0, 100000]);
+  if (window.hasOwnProperty('crypto') && typeof window.crypto.getRandomValues === 'function') {
     window.crypto.getRandomValues(array);
-    console.log("works");
+    console.log('works');
   } else {
-    for (let i = 0; i < 1000; i++) {
+    for (var i = 0; i < 1000; i++) {
       array[i] = Math.floor(Math.random() * 100000) + 1;
     }
   }
